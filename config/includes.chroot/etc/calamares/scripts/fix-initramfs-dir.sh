aa30b200700f0894877a6af06f586ae98572aca8
XiaoDoesStuff <xiao@Laptop-Xiao.localdomain>
2026-05-19 00:09:23 -0500

    cleanup of not needed files and scripts

--- config/includes.chroot/etc/calamares/scripts/fix-initramfs-dir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-ROOT=$(mount | grep calamares-root | awk '{print $3}')
-mkdir -p "$ROOT/etc/initramfs-tools/conf.d"
--- config/includes.chroot/etc/calamares/scripts/postinstall-plymouth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Set Plymouth theme inside the installed system
-plymouth-set-default-theme moobian
-
-# Rebuild initramfs inside installed system
-update-initramfs -u
